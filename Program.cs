5af6c25dd70769b7734a730313b993657eb3b8f5
Nii-Quaye <niiquaye.ashie.7@gmail.com>
2023-01-29 01:33:17 +0000

    A Csharp transaction stream
    A Csharp code that streams transactions in a console, using JSON.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+class Stream
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        dynamic stream = await client.GetStreamAsync("https://api.whale-alert.io/v1/transactions?api_key=your-api-key");
+        
+        using (var reader = new StreamReader(stream))
+        {
+            while (!reader.EndOfStream)
+            {
+                var transactions = reader.ReadLine();
+                dynamic json = JsonConvert.DeserializeObject(transactions);
+                Console.WriteLine(json);
+
+            }
+        }
+        Console.ReadLine();
+    }
+}
